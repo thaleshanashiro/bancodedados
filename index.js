91f36dce151e52fd3b87bb59111d97a061269c0b
thaleshanashiro <73593558+thaleshanashiro@users.noreply.github.com>
2021-06-21 16:16:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 app.use(express.json());
 
-//permissoes
-var cors = require ('cors');
+// Permissões
+var cors = require('cors');
 app.use(cors());
 
-//porta que estou ouvindo
+// Porta que eu estou ouvindo
 app.listen(process.env.PORT || 3000);
 
-app.get('/',function (req, res) {
-res.send("<h2>Times do campeonato brasileiro 2021, serie A:</h2>");
-} 
-  );
+app.get('/', 
+    function (req, res){    
+        res.send("Hello World");
+    }
+);
 
- //Array de um object com os nomes dos times e o Estado de cada um 
-const times = [ 
+app.get('/hello',
+function (req, res){    
+    res.send("Hello de Novo");
+    }
+)
+
+const mensagens = [ 
    {time: "Corinthians ", estado: " Sao Paulo " }, 
    {time: "Santos ", estado: " Sao Paulo" }, 
    {time: "Sao Paulo ", estado: " Sao Paulo "},
@@ -39,49 +45,141 @@ const times = [
    
   ];
 
-//imprime o banco de dados
-app.get('/times',function(req, res)  {
- res.send(times.filter(Boolean)); 
-  }
-    );
+app.get('/mensagens',
+    function(req, res){
+        // res.send(mensagens);
+        res.send(mensagens.filter(Boolean));
+    }
+);
 
-//busca no banco de dados usando o id, ou seja, busca cada linha da matriz em especifico
-  app.get('/times/:id', function(req, res){
-     const id = req.params.id - 1;
-     const mensagem = times [id];
-     if (!mensagem){
-       res.send("mensagem nao encontrada");
-            }
-            else {
-              res.send(mensagem);
-            }
-  }
-  );
+app.get('/mensagens/:id',
+    function(req, res){
+        const id = req.params.id - 1;
+        const mensagem = mensagens[id];
 
-  //adciona um time na matriz 
-  app.post('/times', (req,res) =>{
-       console.log(req.body.mensagem);
-       const mensagem = req.body.mensagem;
-       times.push(mensagem);
-       res.send("Adcionar um time")
-  }
-  );
+        if (!mensagem){
+            res.send("Mensagem não encontrada");
+        } else {
+            res.send(mensagem);
+        }
+    }
+)
 
-  //troca um time da matriz por outro
-  app.put('/times/:id', 
-     (req, res) => {
-       const id = req.params.id -1;
-       const mensagem = req.body.mensagem;
-       times[id] = mensagem;
-       res.send("time atulizado com sucesso")
-     }
-  )
-  
-  //deleta um time da matriz
-  app.delete('/times/:id',
-  (req, res) => {
-     const id = req.params.id -1;
-     delete times[id];
-     res.send("time removida com sucesso");
-  }
-  );
+app.post('/mensagens', 
+    (req, res) => {
+        console.log(req.body.mensagem);
+        const mensagem = req.body.mensagem;
+        mensagens.push(mensagem);
+        res.send("criar uma mensagem.")
+    }
+);
+
+app.put('/mensagens/:id',
+    (req, res) => {
+        const id = req.params.id - 1;
+        const mensagem = req.body.mensagem;
+        mensagens[id] = mensagem;        
+        res.send("Mensagem atualizada com sucesso.")
+    }
+)
+
+app.delete('/mensagens/:id', 
+    (req, res) => {
+        const id = req.params.id - 1;
+        delete mensagens[id];
+
+        res.send("Mensagem removida com sucesso");
+    }
+);
+
+/*
+  Daqui para baixo, uso o banco de dados MongoDB
+*/
+
+const mongodb = require('mongodb')
+const password = process.env.PASSWORD || "asdf";
+console.log(password);
+
+const connectionString = `mongodb+srv://admin:${password}@cluster0.fhdc2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const options = { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+};
+
+(async()=>{
+    const client = await mongodb.MongoClient.connect(connectionString, options);
+    const db = client.db('myFirstDatabase');
+    const mensagens = db.collection('mensagens');
+    console.log(await mensagens.find({}).toArray());
+
+    app.get('/database',
+        async function(req, res){
+        // res.send(mensagens);
+        res.send(await mensagens.find({}).toArray());
+    }
+);
+
+app.get('/database/:id',
+    async function(req, res){
+        const id = req.params.id;
+        const mensagem = await mensagens.findOne(
+            {_id : mongodb.ObjectID(id)}
+        );
+        console.log(mensagem);
+        if (!mensagem){
+            res.send("Mensagem não encontrada");
+        } else {
+            res.send(mensagem);
+        }
+    }
+);
+
+app.post('/database', 
+    async (req, res) => {
+        console.log(req.body);
+        const mensagem = req.body;
+        
+        delete mensagem["_id"];
+
+        mensagens.insertOne(mensagem);        
+        res.send("criar uma mensagem.");
+    }
+);
+
+app.put('/database/:id',
+    async (req, res) => {
+        const id = req.params.id;
+        const mensagem = req.body;
+
+        console.log(mensagem);
+
+        delete mensagem["_id"];
+
+        const num_mensagens = await mensagens.countDocuments({_id : mongodb.ObjectID(id)});
+
+        if (num_mensagens !== 1) {
+            res.send('Ocorreu um erro por conta do número de mensagens');
+            return;
+        }
+
+        await mensagens.updateOne(
+            {_id : mongodb.ObjectID(id)},
+            {$set : mensagem}
+        );
+        
+        res.send("Mensagem atualizada com sucesso.")
+    }
+)
+
+app.delete('/database/:id', 
+    async (req, res) => {
+        const id = req.params.id;
+        
+        await mensagens.deleteOne({_id : mongodb.ObjectID(id)});
+
+        res.send("Mensagem removida com sucesso");
+    }
+);
+
+})();
